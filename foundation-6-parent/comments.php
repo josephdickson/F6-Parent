8d538a5e2e613df056834c4f6d31d3f75a0f040d
Joseph Dickson <joseph.e.dickson@gmmail.com>
2016-01-21 17:12:01 -0800

    Edits to Comments
    edited comments.php to correct some bad function tags.

--- foundation-6-parent/comments.php
+++ foundation-6-parent/comments.php
@@ -29,9 +29,9 @@ if ( post_password_required() ) {
 
 		$comments_args = array(
 			// change the title of send button 
-			'label_submit'=>'Send a Welcome Message',
+			'label_submit'=>'Submit Comment',
 			// change the title of the reply section
-			'title_reply'=>'Send a Welcome Message',
+			'title_reply'=>'Comments',
 			// remove "Text or HTML to be displayed after the set of comment fields"
 			'comment_notes_after' => '',
 			// redefine your own textarea (the comment body)
@@ -52,7 +52,7 @@ if ( post_password_required() ) {
 				<h3 class="comments-title">
 					<?php
 						printf( // WPCS: XSS OK.
-							esc_html( _nx( 'Welcome Message for President-designate Melvin L. Oliver', 'Welcome Messages for President-designate Melvin L. Oliver', get_comments_number(), 'comments title', 'President-designateial-transition' ) ),
+							esc_html( _nx( 'Comment', 'Comments', get_comments_number(), 'comments title', 'foundation-6-parent' ) ),
 							number_format_i18n( get_comments_number() ),
 							'<span>' . get_the_title() . '</span>'
 						);
@@ -61,11 +61,11 @@ if ( post_password_required() ) {
 
 				<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // Are there comments to navigate through? ?>
 				<nav id="comment-nav-above" class="navigation comment-navigation" role="navigation">
-					<h2 class="screen-reader-text"><?php esc_html_e( 'Comment navigation', 'President-designateial-transition' ); ?></h2>
+					<h2 class="screen-reader-text"><?php esc_html_e( 'Comment navigation', 'foundation-6-parent' ); ?></h2>
 					<div class="nav-links">
 
-						<div class="nav-previous"><?php previous_comments_link( esc_html__( 'Older Comments', 'President-designateial-transition' ) ); ?></div>
-						<div class="nav-next"><?php next_comments_link( esc_html__( 'Newer Comments', 'President-designateial-transition' ) ); ?></div>
+						<div class="nav-previous"><?php previous_comments_link( esc_html__( 'Older Comments', 'foundation-6-parent' ) ); ?></div>
+						<div class="nav-next"><?php next_comments_link( esc_html__( 'Newer Comments', 'foundation-6-parent' ) ); ?></div>
 
 					</div><!-- .nav-links -->
 				</nav><!-- #comment-nav-above -->
@@ -82,11 +82,11 @@ if ( post_password_required() ) {
 
 				<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // Are there comments to navigate through? ?>
 				<nav id="comment-nav-below" class="navigation comment-navigation" role="navigation">
-					<h2 class="screen-reader-text"><?php esc_html_e( 'Comment navigation', 'President-designateial-transition' ); ?></h2>
+					<h2 class="screen-reader-text"><?php esc_html_e( 'Comment navigation', 'foundation-6-parent' ); ?></h2>
 					<div class="nav-links">
 
-						<div class="nav-previous"><?php previous_comments_link( esc_html__( 'Older Comments', 'President-designateial-transition' ) ); ?></div>
-						<div class="nav-next"><?php next_comments_link( esc_html__( 'Newer Comments', 'President-designateial-transition' ) ); ?></div>
+						<div class="nav-previous"><?php previous_comments_link( esc_html__( 'Older Comments', 'foundation-6-parent' ) ); ?></div>
+						<div class="nav-next"><?php next_comments_link( esc_html__( 'Newer Comments', 'foundation-6-parent' ) ); ?></div>
 
 					</div><!-- .nav-links -->
 				</nav><!-- #comment-nav-below -->
@@ -99,7 +99,7 @@ if ( post_password_required() ) {
 			// If comments are closed and there are comments, let's leave a little note, shall we?
 			if ( ! comments_open() && get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) : ?>
 
-				<p class="no-comments"><?php esc_html_e( 'Comments are closed.', 'President-designateial-transition' ); ?></p>
+				<p class="no-comments"><?php esc_html_e( 'Comments are closed.', 'foundation-6-parent' ); ?></p>
 			<?php
 			endif;
 ?>
