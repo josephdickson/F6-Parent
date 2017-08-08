9a3746a01c2d3f9efffd7c70d5d9085a766cb1be
Joseph Dickson <joseph.e.dickson@gmmail.com>
2017-08-08 13:39:19 -0700

    Featured Image
    Restored featured image to content-page.php

--- foundation-6-Pitzer-College/template-parts/content-page.php
+++ foundation-6-Pitzer-College/template-parts/content-page.php
@@ -11,6 +11,21 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
+			<?php if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned to it.
+				echo '<div class="row">';
+				echo '<div class="small-12 featured-image">';
+				the_post_thumbnail();
+				echo '<span class="featured-caption">' . get_post(get_post_thumbnail_id())->post_excerpt . '</span>'; // Grabs Caption associated with the Media File / Image
+				echo '</div>';
+				echo '</div>';
+			}
+
+			else { // Do nothing, you can place code here for a fallback when there isn't a featured image set on the page
+
+			}
+			?>
+
+
 			<?php
 
 				/* If a static front page don't display yoast plugin's bread crumbs */
@@ -28,6 +43,7 @@
 				}
 				
 			?>
+
 
 			<header id="content" class="entry-header">
 				<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
