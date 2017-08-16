afe62a0158daef13e5cc0e1bcff38d963031f764
Joseph Dickson <joseph.e.dickson@gmmail.com>
2017-08-16 14:15:40 -0700

    Parent adjustments -
    Javascript update to fix conflict with conditional Gravity forms not
    displaying.
    
    Media Query added for condensed Printing results.

--- foundation-6-Pitzer-College/js/app.js
+++ foundation-6-Pitzer-College/js/app.js
@@ -1,18 +1,19 @@
-jQuery(document).ready(function(){
+function sidebarMenu( jQuery ) {
+	// Check that foundation is ready
 	$(document).foundation();
+	
 	// Adds display block to active Sidebar accordion so it loads open
 	$(".active").parent().css("display", "block");
     
-    // displays quicklinks sub menus set to display:none in app.css to avoid loading glitch
-    $(".top-bar-quicklinks .menu-item-has-children li").css("display", "block"); 
+	// displays quicklinks sub menus set to display:none in app.css to avoid loading glitch
+	$(".top-bar-quicklinks .menu-item-has-children li").css("display", "block"); 
     
-    // displays sidebar sub menus set to display:none in app.css to avoid loading glitch
-    $("ul#sidebar-menu .menu-item-has-children li").css("display", "block"); 
+	// displays sidebar sub menus set to display:none in app.css to avoid loading glitch
+	$("ul#sidebar-menu .menu-item-has-children li").css("display", "block"); 
     
-    // displays Google search box set to display none in app.css to avoid loading glitch
-    $(".input-group").css("display", "block");
-    
-    // remove class of active if the menu item is located in the same site as the menu
-    $( "#quicklinks-menu .is-dropdown-submenu-parent" ).removeClass( "active" );
-    //   $( "#quicklinks-menu .submenu.menu.vertical.is-dropdown-submenu.first-sub").css("display", "none");
-});
+	// remove class of active if the menu item is located in the same site as the menu
+	$( "#quicklinks-menu .is-dropdown-submenu-parent" ).removeClass( "active" );
+	//   $( "#quicklinks-menu .submenu.menu.vertical.is-dropdown-submenu.first-sub").css("display", "none");
+};
+
+$( document ).ready( sidebarMenu );
